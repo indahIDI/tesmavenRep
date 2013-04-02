3b20c058d87d4db6f2d790c57ee11750311241c3
Indah <indah@pcr.ac.id>
2013-04-01 18:46:40 -0700

    ini buat kelas pertama kali

---
+++ src/main/java/id/ac/pcr/tesmaven/App.java
@@ -0,0 +1,13 @@
+package id.ac.pcr.tesmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
